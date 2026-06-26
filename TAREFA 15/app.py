cb2ded11408052641534fd51ff1dac8627a75b13
willianny <ritchellywillianny@gmail.com>
2026-06-26 17:54:56 -0300

    teste

---
+++ TAREFA 15/app.py
@@ -0,0 +1,106 @@
+from flask import Flask, redirect, url_for, session, request, render_template
+from authlib.integrations.flask_client import OAuth
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = "development"
+
+oauth = OAuth(app)
+
+oauth.register(
+    name='suap',
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("CLIENT_SECRET"),
+    api_base_url='https://suap.ifrn.edu.br/api/',
+    access_token_url='https://suap.ifrn.edu.br/o/token/',
+    authorize_url='https://suap.ifrn.edu.br/o/authorize/',
+)
+
+@app.before_request
+def set_token():
+    if "suap_token" in session:
+        oauth.suap.token = session["suap_token"]
+
+# 🔐 LOGIN TOKEN
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    return oauth.suap.authorize_redirect(redirect_uri)
+
+
+@app.route('/login/authorized')
+def auth():
+    token = oauth.suap.authorize_access_token()
+    session['suap_token'] = token
+    return redirect(url_for('perfil'))
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+# 🔧 USUÁRIO
+def get_usuario():
+    resposta = oauth.suap.get('rh/meus-dados')
+    return resposta.json()
+
+
+# 🔧 BOLETIM
+def get_boletim(ano, periodo):
+
+    url = f"ensino/meu-boletim/{ano}/{periodo}/"
+
+    resposta = oauth.suap.get(url)
+
+    return resposta.json().get("results", [])
+
+
+# 🏠 HOME
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# 👤 PERFIL
+@app.route('/perfil')
+def perfil():
+
+    if 'suap_token' not in session:
+        return redirect(url_for('index'))
+
+    usuario = get_usuario()
+
+    return render_template(
+        'user.html',
+        user_data=usuario
+    )
+
+
+# 📚 BOLETIM
+@app.route('/boletim')
+def boletim():
+
+    if 'suap_token' not in session:
+        return redirect(url_for('index'))
+
+    usuario = get_usuario()
+
+    ano = request.args.get("ano", 2024)
+    periodo = request.args.get("periodo", 1)
+
+    dados = get_boletim(ano, periodo)
+
+    return render_template(
+        "boletim.html",
+        user_data=usuario,
+        boletim=dados
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
